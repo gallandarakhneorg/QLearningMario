8cad413061f0cd350e1105d487d4dd9487fee4ec
Benoît CORTIER <benoit.cortier@gmail.com>
2015-11-10 18:47:00 +0100

    Added Block default hitbox.

--- src/mario/entity/Block.java
+++ src/mario/entity/Block.java
@@ -1,12 +1,14 @@
 package mario.entity;
 
 import mario.common.BlockType;
+import mario.common.Hitbox;
 
 public class Block extends Entity {
 	private BlockType type;
 	
 	public Block(BlockType type) {
 		this.type = type;
+		this.currentHitbox = new Hitbox(0, 0, 1, 1);
 	}
 	
 	public BlockType getType() {
