dd1c54ee17b46566308012ddb6d70360fd8314c9
Benoît CORTIER <benoit.cortier@gmail.com>
2015-11-10 16:40:12 +0100

    Added Entity hitbox.

--- src/mario/entity/Entity.java
+++ src/mario/entity/Entity.java
@@ -4,9 +4,11 @@ import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
 
 import javafx.geometry.Point2D;
+import mario.common.Hitbox;
 
 public class Entity {
 	Point2D location;
+	Hitbox hitbox;
 	
 	public void setLocation(Point2D location) {
 		this.location = location;
@@ -19,5 +21,9 @@ public class Entity {
     public double distance(Entity entity) {
         return sqrt(pow(this.location.getX() - entity.getLocation().getX(), 2)
                 + pow(this.location.getY() - entity.getLocation().getY(), 2));
+    }
+    
+    public Hitbox getHitbox() {
+    	return this.hitbox;
     }
 }
