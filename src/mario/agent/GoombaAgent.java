519496d8698c951d8513abc177852acb016056f7
Benoît CORTIER <benoit.cortier@gmail.com>
2015-11-19 14:22:23 +0100

    Updated GoombaAgent.

--- src/mario/agent/GoombaAgent.java
+++ src/mario/agent/GoombaAgent.java
@@ -1,7 +1,5 @@
 package mario.agent;
 
-import java.util.List;
-
 import javafx.geometry.Point2D;
 import mario.entity.Enemy;
 import mario.entity.Entity;
@@ -13,16 +11,14 @@ public class GoombaAgent extends Agent<Goomba> {
 	}
 
 	@Override
-    public void update() {
-        List<Entity> entities = getBody().getPerception();
-        
-        if (getBody().getVelocity() == Point2D.ZERO) {
-        	getBody().askAcceleration(new Point2D(-4.8f, 0));
+    public void update() {       
+        if (getBody().getVelocity().getX() == 0.f) {
+        	getBody().askAcceleration(new Point2D(-Goomba.maxAcceleration.getX(), 0));
             return;
         }
-
-        for (Entity entity : entities) {
-            if (entity instanceof Enemy && entity.collide(getBody())) {
+        
+        for (Entity entity : getBody().getPerception()) {
+            if (entity instanceof Enemy && getBody().collide(entity)) {
                 if (getBody().getVelocity().getX() < 0) {
                 	getBody().askAcceleration(new Point2D(Goomba.maxAcceleration.getX(), 0f));
                 } else {
