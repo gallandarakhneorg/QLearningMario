f1dd0ba0b0481f9ff235cbc0be1269d1fe4adbff
Benoît CORTIER <benoit.cortier@gmail.com>
2015-11-19 14:24:26 +0100

    Added empty MarioAgent.

---
+++ src/mario/agent/MarioAgent.java
@@ -0,0 +1,14 @@
+package mario.agent;
+
+import mario.entity.MarioBody;
+
+public class MarioAgent extends Agent<MarioBody> {
+	public MarioAgent(MarioBody body) {
+		super(body);
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+	}
+}
