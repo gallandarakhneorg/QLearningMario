021b3bf5a0bd0e6522bffe20eea40096175d4d68
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-11-24 17:54:25 +0100

    Replaced System.out by Logger

--- src/main/java/fr/utbm/tc/qlearningmario/mario/Scheduler.java
+++ src/main/java/fr/utbm/tc/qlearningmario/mario/Scheduler.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Logger;
+
+import org.arakhne.afc.vmutil.locale.Locale;
 
 import fr.utbm.tc.qlearningmario.mario.agent.Agent;
 import fr.utbm.tc.qlearningmario.mario.agent.GoombaAgent;
@@ -18,13 +21,15 @@ public class Scheduler implements Runnable, Observer {
     private boolean running = true;
     private int updatesPerSecond = 60;
     
+    private final Logger log = Logger.getLogger(Scheduler.class.getName());
+    
     public Scheduler(World world) {
         this.world = world;
     }
 
     @Override
 	public void run() {
-    	System.out.println("Scheduler started."); //$NON-NLS-1$
+    	this.log.info(Locale.getString(Scheduler.this.getClass(), "scheduler.started")); //$NON-NLS-1$
     	
     	long start_millis;
     	long elapsed_millis;
@@ -45,13 +50,12 @@ public class Scheduler implements Runnable, Observer {
 	            try {
             		Thread.sleep(sleep_millis);
 				} catch (InterruptedException e) {
-					// FIXME: Never call the printStackTrace => use logger
-					e.printStackTrace();
+					this.log.severe(e.toString());
 				}
             }
         }
-        
-        System.out.println("Scheduler ended."); //$NON-NLS-1$
+
+        this.log.info(Locale.getString(Scheduler.this.getClass(), "scheduler.ended")); //$NON-NLS-1$
     }
     
     public void stop() {
@@ -67,11 +71,11 @@ public class Scheduler implements Runnable, Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		if (arg instanceof Goomba) {
-			System.out.println("Added GoombaAgent."); //$NON-NLS-1$
+			this.log.info(Locale.getString(Scheduler.this.getClass(), "added.goomba")); //$NON-NLS-1$
 			GoombaAgent agent = new GoombaAgent((Goomba) arg);
 			this.agents.add(agent);
 		} else if (arg instanceof MarioBody) {
-			System.out.println("Added MarioAgent."); //$NON-NLS-1$
+			this.log.info(Locale.getString(Scheduler.this.getClass(), "added.mario")); //$NON-NLS-1$
 			MarioAgent agent = new MarioAgent((MarioBody) arg);
 			this.agents.add(agent);
 		}
