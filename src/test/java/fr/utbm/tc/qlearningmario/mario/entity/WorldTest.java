246f01b5f38c62c2ecf9c2923967b48409bb80f4
Benoît CORTIER <benoit.cortier@gmail.com>
2015-11-26 20:04:14 +0100

    Added a simple template for unit tests with junit.

---
+++ src/test/java/fr/utbm/tc/qlearningmario/mario/entity/WorldTest.java
@@ -0,0 +1,15 @@
+package fr.utbm.tc.qlearningmario.mario.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class WorldTest {
+
+	@Test
+	public void simpleTest() {
+		assertTrue(true);
+	}
+
+}
