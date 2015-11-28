16c06671ae688256acda03d1ff69eb8e1e0ed3b4
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2015-11-28 18:34:43 +0100

    Corrected an error in MarioBodyTest

--- src/test/java/fr/utbm/tc/qlearningmario/mario/entity/MarioBodyTest.java
+++ src/test/java/fr/utbm/tc/qlearningmario/mario/entity/MarioBodyTest.java
@@ -1,6 +1,6 @@
 package fr.utbm.tc.qlearningmario.mario.entity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -8,16 +8,16 @@ import fr.utbm.tc.qlearningmario.mario.common.MarioState;
 
 @SuppressWarnings("all")
 public class MarioBodyTest {
- 
+
 	@Test
 	public void test() {
 		MarioBody mbody = new MarioBody();
 		assertEquals(1, mbody.getHealth());
-		assertEquals(MarioState.SmallMario, mbody.getState());
-		
+		assertEquals(MarioState.SMALL_MARIO, mbody.getState());
+
 		mbody.damage(mbody.getMaxHealth() + 1);
 		assertEquals(0, mbody.getHealth());
-		
+
 		mbody.heal(mbody.getMaxHealth() + 1);
 		assertEquals(mbody.getMaxHealth(), mbody.getHealth());
 	}
