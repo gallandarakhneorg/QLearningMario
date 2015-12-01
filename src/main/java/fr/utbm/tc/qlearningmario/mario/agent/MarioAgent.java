0829b82438ad6c21e868735bdd2bb171ec6118ac
Benoît CORTIER <benoit.cortier@gmail.com>
2015-12-01 11:43:07 +0100

    Added javadoc in MarioAgent class.

--- src/main/java/fr/utbm/tc/qlearningmario/mario/agent/MarioAgent.java
+++ src/main/java/fr/utbm/tc/qlearningmario/mario/agent/MarioAgent.java
@@ -17,27 +17,52 @@
  *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  *******************************************************************************/
+
 package fr.utbm.tc.qlearningmario.mario.agent;
+
+import org.arakhne.afc.vmutil.locale.Locale;
 
 import fr.utbm.tc.qlearningmario.mario.entity.MarioBody;
 import fr.utbm.tc.qlearningmario.qlearning.QAction;
 import fr.utbm.tc.qlearningmario.qlearning.QLearning;
 import javafx.geometry.Point2D;
 
+/** The MarioBody's corresponding agent.
+ *
+ * <p>It makes use of the Q-Learning engine.
+ *
+ * @author Benoît CORTIER
+ * @mavengroupid fr.utbm.tc
+ * @mavenartifactid QLearningMario
+ */
 public class MarioAgent extends Agent<MarioBody> {
+	private static final int NB_LEARNING_ITERATIONS;
+
+	static {
+		NB_LEARNING_ITERATIONS = Integer.parseInt(Locale.getString(MarioAgent.class, "number.learning.iterations")); //$NON-NLS-1$
+	}
+
 	private MarioProblem problem = new MarioProblem();
+
 	private QLearning<MarioProblem> qlearning = new QLearning<>(this.problem);
 
+	/** Initialize the agent with the given MarioBody.
+	 *
+	 * @param body : a MarioBody.
+	 */
 	public MarioAgent(MarioBody body) {
 		super(body);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void live() {
 		super.live();
 
 		this.problem.translateCurrentState(getBody(), getBody().getPerception());
-		this.qlearning.learn(5);
+		this.qlearning.learn(NB_LEARNING_ITERATIONS);
 
 		QAction qAction = this.qlearning.getBestAction(this.problem.getCurrentState());
 		MarioProblem.Action action = MarioProblem.Action.fromQAction(qAction);
